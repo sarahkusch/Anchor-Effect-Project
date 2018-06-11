2028d786a7024d581167c011c0f892fb340b23e2
Inga Kramarek <ikramarek@uos.de>
2018-06-11 14:19:32 +0200

    Pilot experiment updated, type 2 questions still missing

---
+++ experiments/pilot/AnchorPilot/scripts/helpers.js
@@ -0,0 +1,17 @@
+// insert any functions that are useful throughout the experiment here
+var shuffleComb = function(comb) {
+    // while this one is trivial, this just to show that we CAN define a function here
+    return _.shuffle(comb);
+};
+
+function coinFlip(head, tail) {
+  return (Math.floor(Math.random() * 2) == 0) ? head : tail;
+};
+
+function anchor(high, low) {
+  if (coinFlip(high, low) == high) {
+    question = question_high
+  } else {
+    question = question_low
+  }
+}
---
+++ experiments/pilot/AnchorPilot/trial_info/practice_trials.js
@@ -0,0 +1,8 @@
+var practice_trials = [
+	{question: "Is the distance between Hamburg and Munich higher or lower than " + coinFlip(70,1000) + " km?",
+	 option1: "higher",
+	 option2: "lower"},
+	{question: "Did Karl Marx die before or after " + coinFlip(1805,1920) + " ?",
+	 option1: "before",
+	 option2: "after"}
+];
